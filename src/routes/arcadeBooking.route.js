313feb592762ff3a88b8a0c599f1a7754278d3e0
Navindu Chathuranga <125124792+spynav@users.noreply.github.com>
2024-02-04 15:01:15 +0530

    BookingCRUD
    Booking CRUD Complited

--- src/routes/arcadeBooking.route.js
+++ src/routes/arcadeBooking.route.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 const arcadeBookingController = require("../controllers/arcadeBooking.controller");
 
-router.get("/api/getarcadebookings", arcadeBookingController.getArcadeBookings);
-router.post("/api/addarcadeBooking", arcadeBookingController.addArcadeBooking);
+router.get("/api/getarcadebookings", arcadeBookingController.getArcadeBooking);
+router.post("/api/addarcadebooking", arcadeBookingController.addArcadeBooking);
+router.put("/api/updatearcadebooking/:id", arcadeBookingController.updateArcadeBooking);
+router.delete("/api/deletearcadebooking/:id", arcadeBookingController.deleteArcadeBooking);
+
 
 module.exports = router;
