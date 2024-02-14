f24da740f973d2f259c5a138d3b646e49903b3df
Navindu Chathuranga <125124792+spynav@users.noreply.github.com>
2024-02-15 03:48:00 +0530

    api done

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const cors = require("cors");
 
 const arcadeBookingRoutes = require("./src/routes/arcadeBooking.route");
 const discountCardRoutes = require("./src/routes/form.discountcard.route");
+const coachCardRoutes = require("./src/routes/form.coachCard.route");
 
 app.use(express.json());
 app.use(cors());
 
 app.use(arcadeBookingRoutes);
 app.use(discountCardRoutes);
+app.use(coachCardRoutes);
 
 app.listen(8000, () => {
   console.log("Server running on port 8000");
