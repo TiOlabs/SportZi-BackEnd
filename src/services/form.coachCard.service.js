6e86adc5d5392c889ebdcfff7389519564c4522b
Navindu Chathuranga <125124792+spynav@users.noreply.github.com>
2024-02-16 20:14:13 +0530

    ok

--- src/services/form.coachCard.service.js
+++ src/services/form.coachCard.service.js
@@ -1,36 +1,36 @@
-const {PrismaClient} = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 const getCoachCards = async () => {
-    return await prisma.coachAssignDetails.findMany();
+  return await prisma.coachAssignDetails.findMany();
 };
 
 const addCoachCard = async (coachAssignDetails) => {
-    return await prisma.coachAssignDetails.create({
-        data: {
-            ...coachAssignDetails,
-        },
-    });
+  return await prisma.coachAssignDetails.create({
+    data: {
+      ...coachAssignDetails,
+    },
+  });
 };
 
 const updateCoachCard = async (id, coach) => {
-    return await prisma.coachCard.update({
-        where: {id: id},
-        data: {
-            ...coachassigndetails,
-        },
-    });
+  return await prisma.coachCard.update({
+    where: { id: id },
+    data: {
+      ...coachassigndetails,
+    },
+  });
 };
 
 const deleteCoachCard = async (id) => {
-    return await prisma.coachCard.delete({
-        where: {id: id},
-    });
+  return await prisma.coachCard.delete({
+    where: { id: id },
+  });
 };
 
 module.exports = {
-    getCoachCards,
-    addCoachCard,
-    updateCoachCard,
-    deleteCoachCard,
+  getCoachCards,
+  addCoachCard,
+  updateCoachCard,
+  deleteCoachCard,
 };
