2df1ab7c66d408854837ab3e0aeb1e9e31fdfe3c
Navindu Chathuranga <125124792+spynav@users.noreply.github.com>
2024-02-18 12:04:26 +0530

    change to 8000 again

--- server.js
+++ server.js
@@ -17,6 +17,6 @@ app.get("/", (req, res) => {
   res.send("Backend Server is Running");
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(8000, () => {
+  console.log("Server running on port 8000");
 });
