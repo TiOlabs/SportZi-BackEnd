fe9391fa87c8f6cb4761e3ec5d10f673a841f2d0
Thisara-Senarathna <thisarasenarathna0824@gmail.com>
2024-03-05 00:57:59 +0530

    done token athentication

--- src/configs/config.js
+++ src/configs/config.js
@@ -1,12 +1,12 @@
-// config.js
+// // config.js
 
-const crypto = require('crypto');
+// const crypto = require('crypto');
 
-const JWT_SECRET = crypto.randomBytes(32).toString('hex');
-console.log(JWT_SECRET);
+// const JWT_SECRET = crypto.randomBytes(32).toString('hex');
+// console.log(JWT_SECRET);
 
 
-module.exports = {
-    JWT_SECRET: JWT_SECRET
-  };
+// module.exports = {
+//     JWT_SECRET: JWT_SECRET
+//   };
   
