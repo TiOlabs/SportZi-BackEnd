7d385fe99242487eae19766da9717e168de9e694
kanishka udayanga <119073238+mskanishkaudayanga@users.noreply.github.com>
2024-03-10 12:28:43 +0530

    payment status succes

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -9,4 +9,24 @@ const getpaymentditails = async (req, res) => {
   }
 };
 
-module.exports = { getpaymentditails };
+// const postpaymentStatus = async (req, res) => {
+//   try {
+//     const paymentStatus = await paymentcontroller.postPaymentDitails();
+//     res.status(200).json(paymentStatus);
+//   } catch (error) {
+//     res.status(500).json({ messageee: error.message });
+//   }
+// };
+
+async function postpaymentStatus(req, res, next) {
+  const { paymentId } = req.body;
+  try {
+    await paymentService.updatePaymentStatus(paymentId);
+    res.send("Payment status updated successfully");
+  } catch (error) {
+    console.error("Error updating payment status:", error);
+    res.status(500).send("Error updating payment status");
+  }
+}
+
+module.exports = { getpaymentditails, postpaymentStatus };
