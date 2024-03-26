720977db7a525bb6797ef78620ca33dafd8e9d58
kanishka udayanga <119073238+mskanishkaudayanga@users.noreply.github.com>
2024-03-27 02:16:31 +0530

    player details route create

--- src/middlewares/authorizePlayer.js
+++ src/middlewares/authorizePlayer.js
@@ -1,17 +1,15 @@
+function authorizePlayer(req, res, next) {
+  const { role } = req.user;
 
-
-  function authorizePlayer(req, res, next) {
-    const { role } = req.user;
-  
-    // Check if user is a player
-    if (role !== 'PLAYER') {
-      return res.status(403).json({ message: 'Forbidden: Only players can access this route'});
-    }
-  
-    // Proceed to the next middleware or route handler
-    next();
+  // Check if user is a player
+  if (role !== "PLAYER") {
+    return res
+      .status(403)
+      .json({ message: "Forbidden: Only players can access this route" });
   }
-  
-  
-  module.exports = authorizePlayer; 
-  
+
+  // Proceed to the next middleware or route handler
+  next();
+}
+
+module.exports = authorizePlayer;
