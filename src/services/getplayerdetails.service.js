cda687f2afed1bae2b81d4d828ae0cc0dc8b417c
kanishka udayanga <119073238+mskanishkaudayanga@users.noreply.github.com>
2024-03-27 02:15:08 +0530

    create serverjs file to the get profile by id

---
+++ src/services/getplayerdetails.service.js
@@ -0,0 +1,14 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+const getPlayerdetails = async (user_id) => {
+  return await prisma.user.findUnique({
+    where: {
+      user_id: user_id,
+    },
+    //how to get user table relation data to the this get methord
+  });
+};
+module.exports = {
+  getPlayerdetails,
+};
