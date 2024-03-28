9605bd678c13412cdf7614e7935fd414ab066df2
kanishka udayanga <119073238+mskanishkaudayanga@users.noreply.github.com>
2024-03-28 18:29:55 +0530

    done

--- src/services/coachAssignArcade.service.js
+++ src/services/coachAssignArcade.service.js
@@ -1,44 +1,40 @@
-const {PrismaClient} = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 const getCoachCards = async () => {
-    return await prisma.coachAssignDetailsForArcade.findMany({
-        include: {
-          coach: {
-            include:{
-                user:true
-            }
-          }
-        } 
-      });
+  return await prisma.coachAssignDetailsForArcade.findMany({
+    include: {
+      coach: {},
+    },
+  });
 };
 
 const addCoachCard = async (coachAssignDetailsForArcade) => {
-    return await prisma.coachAssignDetailsForArcade.create({
-        data: {
-            ...coachAssignDetailsForArcade,
-        },
-    });
+  return await prisma.coachAssignDetailsForArcade.create({
+    data: {
+      ...coachAssignDetailsForArcade,
+    },
+  });
 };
 
 const updateCoachCard = async (id, coach) => {
-    return await prisma.coachAssignDetailsForArcade.update({
-        where: {id: id},
-        data: {
-            ...coachAssignDetailsForArcade,
-        },
-    });
+  return await prisma.coachAssignDetailsForArcade.update({
+    where: { id: id },
+    data: {
+      ...coachAssignDetailsForArcade,
+    },
+  });
 };
 
 const deleteCoachCard = async (id) => {
-    return await prisma.coachAssignDetailsForArcade.delete({
-        where: {id: id},
-    });
+  return await prisma.coachAssignDetailsForArcade.delete({
+    where: { id: id },
+  });
 };
 
 module.exports = {
-    getCoachCards,
-    addCoachCard,
-    updateCoachCard,
-    deleteCoachCard,
+  getCoachCards,
+  addCoachCard,
+  updateCoachCard,
+  deleteCoachCard,
 };
