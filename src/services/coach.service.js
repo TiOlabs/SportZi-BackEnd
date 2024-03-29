2880b163191fe0e5a1ae108257bb0f44b4d2bc34
Navindu Chathuranga <125124792+spynav@users.noreply.github.com>
2024-03-29 15:39:30 +0530

    little change in coach signup

--- src/services/coach.service.js
+++ src/services/coach.service.js
@@ -2,14 +2,13 @@ const { PrismaClient, Role } = require("@prisma/client");
 const prisma = new PrismaClient();
 const bcrypt = require("bcrypt");
 
-
 const getCoaches = async () => {
   return await prisma.coach.findMany({
     include: {
-      user: true ,
-      sport:true,
-      coachFeedbacks:true
-    }
+      user: true,
+      sport: true,
+      coachFeedbacks: true,
+    },
   });
 };
 
@@ -30,17 +29,16 @@ const getCoaches = async () => {
 //     });
 //   };
 
-
 const deleteCoach = async (coachId) => {
   try {
     // First, find the coach based on the provided coachId
     const coach = await prisma.coach.findUnique({
       where: {
-        coach_id: coachId
+        coach_id: coachId,
       },
       include: {
-        user: true // Include the associated user
-      }
+        user: true, // Include the associated user
+      },
     });
 
     if (!coach) {
@@ -50,15 +48,15 @@ const deleteCoach = async (coachId) => {
     // Delete the coach
     await prisma.coach.delete({
       where: {
-        coach_id: coachId
-      }
+        coach_id: coachId,
+      },
     });
 
     // Delete the associated user
     await prisma.user.delete({
       where: {
-        user_id: coach.user.user_id
-      }
+        user_id: coach.user.user_id,
+      },
     });
 
     return coach;
@@ -67,105 +65,95 @@ const deleteCoach = async (coachId) => {
   }
 };
 
-
-
-
-
 //new services
 
-const addCoach = async (req,res,coach) => {
-
-  // Check if the email is already registered
-  const existingUser = await prisma.user.findUnique({
-    where: {
-      email:coach.email
-    },
-  });
-  if (existingUser) {
-    return res.status(400).json({ error: "Email is already registered" });
-  }
-
-
-  const hashedPassword = await bcrypt.hash(coach.password, 10); // Hash the password
-
-  async function generateUserID() {
-    const userCount = await prisma.coach.count(); // Get the count of existing users
-    const paddedID = String(userCount + 1).padStart(5, "0"); // Pad numeric ID with zeros to ensure it's at least 4 digits long
-    return `C${paddedID}`; 
-  }
-
-  const newCoachID = await generateUserID();
-
-  const newUser =  await prisma.user.create({
-    data: {
-      // ...coach,
-      user_id:newCoachID,
-      firstname:coach.firstname,
-      lastname:coach.lastname,
-      email:coach.email,
-      DOB:coach.DOB,
-      gender:coach.gender,
-      role:Role.COACH,
-      password: hashedPassword,
-    },
-  });
-
-
-
-  async function generateSportID() {
-    const sportCount = await prisma.sport.count(); // Get the count of existing users
-    const paddedID = String(sportCount + 1).padStart(4, "0"); // Pad numeric ID with zeros to ensure it's at least 4 digits long
-    return `SP${paddedID}`; 
-  }
-  const newSportID = await generateSportID();
-
-  //add sport table tuple
-  const newSport = await prisma.sport.create({
-    data:{
-      sport_id:newSportID,
-      sport_name:coach.sport_name
+const addCoach = async (req, res, coach) => {
+  try {
+    // Check if the email is already registered
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        email: coach.email,
+      },
+    });
+    if (existingUser) {
+      return res.status(400).json({ error: "Email is already registered" });
     }
-  })
 
-  const newCoach = await prisma.coach.create({
-    data:{
-      user: {
-        connect: {
-          user_id:newCoachID,
+    const hashedPassword = await bcrypt.hash(coach.password, 10); // Hash the password
+
+    async function generateUserID() {
+      const userCount = await prisma.coach.count(); // Get the count of existing users
+      const paddedID = String(userCount + 1).padStart(5, "0"); // Pad numeric ID with zeros to ensure it's at least 4 digits long
+      return `C${paddedID}`;
+    }
+
+    const newCoachID = await generateUserID();
+
+    const newUser = await prisma.user.create({
+      data: {
+        // ...coach,
+        user_id: newCoachID,
+        firstname: coach.firstname,
+        lastname: coach.lastname,
+        email: coach.email,
+        DOB: coach.DOB,
+        gender: coach.gender,
+        role: Role.COACH,
+        password: hashedPassword,
+      },
+    });
+
+    async function generateSportID() {
+      const sportCount = await prisma.sport.count(); // Get the count of existing users
+      const paddedID = String(sportCount + 1).padStart(4, "0"); // Pad numeric ID with zeros to ensure it's at least 4 digits long
+      return `SP${paddedID}`;
+    }
+    const newSportID = await generateSportID();
+
+    //add sport table tuple
+    const newSport = await prisma.sport.create({
+      data: {
+        sport_id: newSportID,
+        sport_name: coach.sport_name,
+      },
+    });
+
+    const newCoach = await prisma.coach.create({
+      data: {
+        user: {
+          connect: {
+            user_id: newCoachID,
+          },
+        },
+        sport: {
+          connect: {
+            sport_id: newSportID,
+          },
         },
       },
-      sport:{
-        connect:{
-          sport_id:newSportID,
+    });
+
+    const newPhone = await prisma.userPhone.create({
+      data: {
+        phone_number: coach.phone_number,
+
+        user: {
+          connect: {
+            user_id: newCoachID,
+          },
         },
-      }
-    }
-  }) 
+      },
+    });
 
-  const newPhone = await prisma.userPhone.create({
-    data:{
-      phone_number:coach.phone_number,
-
-      user:{
-        connect:{
-          user_id:newCoachID,
-        }
-      }
-    },
-    
-  })
-
-
-  
-
-  res.status(201).json(newUser);
+    res.status(201).json(newUser);
+  } catch (e) {
+    console.log(e);
+  }
 };
 
-
-
-  module.exports = {
-      getCoaches,
-      addCoach,
-      // updateCoach,
-      deleteCoach 
-  } 
+module.exports = {
+  getCoaches,
+  addCoach,
+  // updateCoach,
+  deleteCoach,
+};
