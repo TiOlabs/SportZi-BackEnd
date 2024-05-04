3654f52f4a4085052e82b0e1c694de9b30cf0b69
Wijesinghe W.N.U. <141928283+Niwee990519@users.noreply.github.com>
2024-05-04 22:12:57 +0530

    done

--- src/services/coach.service.js
+++ src/services/coach.service.js
@@ -8,11 +8,35 @@ const getCoaches = async () => {
       user: true,
       sport: true,
       coachFeedbacks: true,
-
     },
   });
 };
 
+const getCoachById = async (coachId) => {
+  return await prisma.coach.findUnique({
+    where: {
+      coach_id: coachId,
+    },
+    include: {
+      user: true,
+      sport: true,
+    },
+  });
+};
+
+const getCoachAvailiability = async (coachId) => {
+  try {
+    const coach = await prisma.availiability.findMany({});
+
+    if (!coach) {
+      throw new Error("Coach not found");
+    }
+
+    return coach;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
 // const addCoach = async (coach) => {
 //   return await prisma.coach.create({
 //     data: {
@@ -38,14 +62,13 @@ const deleteCoach = async (coachId) => {
         coach_id: coachId,
       },
       include: {
-        user:{
-          include:{
-            phone:true,
-          }
+        user: {
+          include: {
+            phone: true,
+          },
         }, // Include the associated user
       },
     });
-    
 
     if (!coach) {
       throw new Error("Coach not found");
@@ -78,112 +101,104 @@ const deleteCoach = async (coachId) => {
     // Delete the associated user
     await prisma.user.delete({
       where: {
-        user_id:coachId,
+        user_id: coachId,
       },
     });
     return coach;
-
   } catch (error) {
     throw new Error(error);
   }
 };
 
-
 const addCoach = async (req, res, coach) => {
   try {
-  async function generateUserID() {
-    const userCount = await prisma.coach.count(); // Get the count of existing users
-    const paddedID = String(userCount + 1).padStart(5, "0"); // Pad numeric ID with zeros to ensure it's at least 4 digits long
-    return `C${paddedID}`;
-  }
+    async function generateUserID() {
+      const userCount = await prisma.coach.count(); // Get the count of existing users
+      const paddedID = String(userCount + 1).padStart(5, "0"); // Pad numeric ID with zeros to ensure it's at least 4 digits long
+      return `C${paddedID}`;
+    }
 
-  async function generateSportID() {
-    const sportCount = await prisma.sport.count(); // Get the count of existing users
-    const paddedID = String(sportCount + 1).padStart(4, "0"); // Pad numeric ID with zeros to ensure it's at least 4 digits long
-    return `SP${paddedID}`;
-  }
+    async function generateSportID() {
+      const sportCount = await prisma.sport.count(); // Get the count of existing users
+      const paddedID = String(sportCount + 1).padStart(4, "0"); // Pad numeric ID with zeros to ensure it's at least 4 digits long
+      return `C${paddedID}`;
+    }
 
-  // Check if the email is already registered
-  const existingUser = await prisma.user.findUnique({
-    where: {
-      email: coach.email,
-    },
-  });
-  if (existingUser) {
-    return res.status(400).json({ message: "Email is already registered" });
-  }
+    // Check if the email is already registered
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        email: coach.email,
+      },
+    });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email is already registered" });
+    }
 
+    const hashedPassword = await bcrypt.hash(coach.password, 10); // Hash the password
 
-  const hashedPassword = await bcrypt.hash(coach.password, 10); // Hash the password
+    const newCoachID = await generateUserID();
 
-  const newCoachID = await generateUserID();
-
-  const newUser = await prisma.user.create({
-    data: {
-      // ...coach,
-      user_id: newCoachID,
-      firstname: coach.firstname,
-      lastname: coach.lastname,
-      email: coach.email,
-      DOB: coach.DOB,
-      gender: coach.gender,
-      role: Role.COACH,
-      password: hashedPassword,
-    },
-  });
-
-  const existingSport = await prisma.sport.findFirst({
-    where: {
-      sport_name: coach.sport_name,
-    },
-  });
-
-
-  let sportID;
-  if (!existingSport) {
-    sportID = await generateSportID();
-
-    //add sport table tuple
-    const newSport = await prisma.sport.create({
+    const newUser = await prisma.user.create({
       data: {
-        sport_id: sportID,
+        // ...coach,
+        user_id: newCoachID,
+        firstname: coach.firstname,
+        lastname: coach.lastname,
+        email: coach.email,
+        DOB: coach.DOB,
+        gender: coach.gender,
+        role: Role.COACH,
+        password: hashedPassword,
+      },
+    });
+
+    const existingSport = await prisma.sport.findFirst({
+      where: {
         sport_name: coach.sport_name,
       },
     });
 
-  } 
-  else {
-    sportID = existingSport.sport_id;
-  }
-  
+    let sportID;
+    if (!existingSport) {
+      sportID = await generateSportID();
 
-  const newCoach = await prisma.coach.create({
-    data: {
-      user: {
-        connect: {
-          user_id: newCoachID,
-        },
-      },
-      sport: {
-        connect: {
+      //add sport table tuple
+      const newSport = await prisma.sport.create({
+        data: {
           sport_id: sportID,
+          sport_name: coach.sport_name,
+        },
+      });
+    } else {
+      sportID = existingSport.sport_id;
+    }
+
+    const newCoach = await prisma.coach.create({
+      data: {
+        user: {
+          connect: {
+            user_id: newCoachID,
+          },
+        },
+        sport: {
+          connect: {
+            sport_id: sportID,
+          },
         },
       },
-    },
-  });
-  
-  const newPhone = await prisma.userPhone.create({
-    data: {
-      phone_number: coach.phone_number,
+    });
 
-      user: {
-        connect: {
-          user_id: newCoachID,
+    const newPhone = await prisma.userPhone.create({
+      data: {
+        phone_number: coach.phone_number,
+
+        user: {
+          connect: {
+            user_id: newCoachID,
+          },
         },
       },
-    },
-  });
-
+    });
 
     res.status(201).json(newUser);
   } catch (e) {
@@ -193,6 +208,8 @@ const addCoach = async (req, res, coach) => {
 
 module.exports = {
   getCoaches,
+  getCoachById,
+  getCoachAvailiability,
   addCoach,
   // updateCoach,
   deleteCoach,
