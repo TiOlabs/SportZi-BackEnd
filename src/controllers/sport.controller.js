de30b5b7da2b764de2d3dc8ad69114a4434ff7f6
Navindu Chathuranga <125124792+spynav@users.noreply.github.com>
2024-05-21 17:17:53 +0530

    Done

--- src/controllers/sport.controller.js
+++ src/controllers/sport.controller.js
@@ -24,6 +24,7 @@ const getSportById = async (req, res) => {
 };
 
 const addSport = async (req, res) => {
+  console.log(req.body);
   try {
     const sport = req.body;
     const newSport = await sportService.addSport(sport);
--- src/services/sport.service.js
+++ src/services/sport.service.js
@@ -1,40 +1,67 @@
-const {PrismaClient} = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 const getSport = async () => {
-    return await prisma.sport.findMany();
-    }
+  return await prisma.sport.findMany();
+};
 const getSportById = async (id) => {
-    return await prisma.sport.findUnique({
-        where: {
-            sport_id: id,
-        },
-    });
-}
+  return await prisma.sport.findUnique({
+    where: {
+      sport_id: id,
+    },
+  });
+};
 const addSport = async (sport) => {
-    return await prisma.sport.create({
-        data: sport,
+  console.log(sport);
+  try {
+    async function generateSportId() {
+      const sports = await prisma.sport.count();
+      const sportId = String(sports + 1).padStart(5, "0");
+      return `S${sportId}`;
+    }
+    const existingSport = await prisma.sport.findUnique({
+      where: {
+        sport_name: sport.sport_name,
+      },
     });
-}
+    console.log(sport);
+    if (existingSport) {
+      return res.status(400).json({ message: "Sport is already registered" });
+    }
+    const newSportID = await generateSportId();
+    console.log(newSportID);
+    console.log(sport.sport_name);
+    const newSport = await prisma.sport.create({
+      data: {
+        sport_id: newSportID,
+        sport_name: sport.sport_name,
+      },
+    });
+    res.status(201).json(newSport);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+};
 const updateSport = async (id, sport) => {
-    return await prisma.sport.update({
-        where: {
-            sport_id: id,
-        },
-        data: sport,
-    });
-}
+  return await prisma.sport.update({
+    where: {
+      sport_id: id,
+    },
+    data: sport,
+  });
+};
 const deleteSport = async (id) => {
-    return await prisma.sport.delete({
-        where: {
-            sport_id: id,
-        },
-    });
-}
+  return await prisma.sport.delete({
+    where: {
+      sport_id: id,
+    },
+  });
+};
 module.exports = {
-    getSport,
-    getSportById,
-    addSport,
-    updateSport,
-    deleteSport,
+  getSport,
+  getSportById,
+  addSport,
+  updateSport,
+  deleteSport,
 };
