34cc071b9f7f472baed3727135194b514debece5
Thisara-Senarathna <thisarasenarathna0824@gmail.com>
2024-06-09 20:44:37 +0530

    display coachFeedbacks BE

--- src/routes/coachFeedbacks.route.js
+++ src/routes/coachFeedbacks.route.js
@@ -8,8 +8,8 @@ const authenticateToken = require("../middlewares/authenticateToken");
 const authorizePlayer = require("../middlewares/authorizePlayer");
 
 
-// router.get("/api/getcoachfeedbacks", coachFeedbacksController.getCoachFeedbacks);
-router.post("/api/addcoachfeedbacks",authenticateToken,authorizePlayer,coachFeedbacksController.addCoachFeedbacks);
+ router.get("/api/getcoachfeedbacks/:coachId", coachFeedbacksController.getCoachFeedbacks);
+router.post("/api/addcoachfeedbacks/:coachId",authenticateToken,coachFeedbacksController.addCoachFeedbacks);
 // router.put("/api/updatecoachfeedbacks/:id", coachFeedbacksController.updateCoachFeedbacks);
 // router.delete("/api/deletecoachfeedbacks/:id", coachFeedbacksController.deleteCoachFeedbacks);
 
