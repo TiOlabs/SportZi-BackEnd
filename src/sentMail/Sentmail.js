2741c8d2196251a7e483fe3b5aa2e0fbb8ae97f3
Navindu Chathuranga <125124792+spynav@users.noreply.github.com>
2024-06-19 02:06:50 +0530

    done

--- src/sentMail/Sentmail.js
+++ src/sentMail/Sentmail.js
@@ -1,11 +1,11 @@
 const nodemailer = require("nodemailer");
 
-const SENDERSMAIL = "arunapriyadarshana004@gmail.com";
+const SENDERSMAIL = "sportzilive@gmail.com";
 const transporter = nodemailer.createTransport({
   service: "gmail", // Use `true` for port 465, `false` for all other ports
   auth: {
-    user: "arunapriyadarshana004@gmail.com",
-    pass: "qghj szuj ywvv idwc",
+    user: "sportzilive@gmail.com",
+    pass: "wknt piws spcl xvpc",
   },
 });
 transporter.verify((error, success) => {
